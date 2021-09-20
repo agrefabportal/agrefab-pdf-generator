aec47a52a8676baa72a141803d3dbafb94623c49
Nate <nw3455@gmail.com>
2021-09-20 00:10:07 -0700

    Initialize ability to create PDF file.

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const pdfkit = require('pdfkit');
+const PDFDocument = require('pdfkit');
+const fs = require('fs');
+
 class Pdf {
     /**
      * Generate PDF documents
@@ -6,5 +8,17 @@ class Pdf {
     constructor() {
 
     }
+    /**
+     * Create test document
+     * @param {string} filePath Path where the file will be saved. Relative to the working directory.
+     */
+    create(name, data) {
+        const doc = new PDFDocument();
+        doc.pipe(fs.createWriteStream(`${name}.pdf`));
+        doc.font('fonts/roboto/Roboto-Regular.ttf')
+            .fontSize(25)
+            .text(data, 100, 100);
+        doc.end();
+    }
 }
-module.exports = Pdf;
+module.exports = new Pdf();
--- test.js
+++ test.js
@@ -1,5 +1,22 @@
 const assert = require('assert')
 const pdf = require('./index.js');
-(async function main() {
+const fs = require('fs');
+const { resolve } = require('path');
 
+(async function main() {
+    testPdfGenerator_createsFile();
 })().catch(error => console.error(error));
+
+function testPdfGenerator_createsFile() {
+    let name = '########TEST########';
+    let path = name + '.pdf';
+    pdf.create(name, '');
+    fs.stat(path, function (err, stat) {
+        assert.equal(err, null, 'PDF generator did not create a file or the naming is off.');
+    });
+    fs.unlink(path, (_) => {
+        fs.stat(path, function (err, stat) {
+            assert.equal(err.code, 'ENOENT', 'File was not deleted');
+        });
+    });
+}
