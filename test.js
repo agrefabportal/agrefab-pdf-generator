02849ad86e3f686be6c53fbceb78e2bdcfc4cab0
Nate <nw3455@gmail.com>
2021-09-20 18:22:48 -0700

    Add tests for file naming conventions
    Specifies the directory and default name for pdf files

--- test.js
+++ test.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 
 (async function main() {
     testPdfGenerator_createsFile();
+    testFileSystemNamingConvention();
 })().catch(error => console.error(error));
 /**
  * Test that the pdf generator creates a file on the local filesystem
@@ -11,6 +12,17 @@ const fs = require('fs');
 async function testPdfGenerator_createsFile() {
     let pdf = new PDFGenerator('testPdfGenerator_createsFile');
     pdf.save().then(deleteFile(pdf.filePath));
+}
+/**
+ * Test filesystem naming conventions.
+ */
+async function testFileSystemNamingConvention() {
+    let pdf1 = new PDFGenerator();
+    assert.equal(pdf1.filePath, 'Untitled.pdf', `Default file name did not match expected.`);
+    let name = 'testFileSystemNamingConvention';
+    let pdf2 = new PDFGenerator(name);
+    assert.equal(pdf2.filePath, name + '.pdf', `File naming convention does not match. Expected filename + .pdf. The pdf has a file path of ${pdf2.filePath}`);
+    pdf2.save().then(deleteFile(pdf2.filePath));
 }
 /**
  * Delete file. This inherently also checks if the file exists and other errors in the delete process.
