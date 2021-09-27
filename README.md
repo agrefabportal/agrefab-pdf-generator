beb684ed2de25edcd003a6fc2f3f9f776bd71425
Nate <nw3455@gmail.com>
2021-09-27 07:28:05 -0700

    Update documentation

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # PDF Generator
 NodeJS library for creating PDF documents.
+
+## Installation
+`npm i agrefab-pdf-generator`
+
+## How to use
+```javascript
+let pdf = new PDFGenerator('Untitled');
+let introduction = 'Introduction text';
+let steps = ['Instructions for step 1', 'Instructions for step 2'];
+let numberId = 'SOP-ALL-001';
+let revisionDate = '8/9/21';
+let effectiveDate = '8/9/21';
+let approvedBy = 'email@example.com';
+let author = 'email@example.com';
+let replaces = '';
+let version = 1;
+pdf.saveGuide({
+        introduction,
+        steps,
+        author,
+        numberId,
+        revisionDate,
+        effectiveDate,
+        approvedBy,
+        replaces,
+        version,
+    });
+```
+See [tests.js](tests.js) for more examples.
+
+## Usage information
+* saveGuide() creates a file on the local filesystem.
+* Each PDFGenerator class represents one instance of a PDF. Do not reuse the same object. Create a new instance to rewrite a file.
+*  saveGuide() returns a promise, so it executes asychronously.
--- test.js
+++ test.js
@@ -1,19 +1,19 @@
 const assert = require('assert');
-const PDFGenerator = require('./index.js');
 const fs = require('fs');
+const PDFGenerator = require('./index.js');
 const PDFParser = require('pdf2json');
 var MOCK_TEXT_LONG = `1.1 Purpose\nTitle:\nAssurance GDS\nRevision Date:\nEffective:\nMM/DD/YY\n    Describe the rationale for this particular document (ensure, describe, control). State the intention/objective of the process/procedure.\nNote: You know you’ve written a solid purpose statement when you can combine the policy statement and the purpose to form a logical what (policy) and why (purpose).\n1.2 Scope\nWhen and to whom the process/procedure applies.\nWho is affected, which areas, which activities, what are the limits (parameters for applying process/procedure).\n1.3 Roles\nList the role and functions of anyone involved in the process/procedure.\nUse verbs and nouns to describe the functions\n1.4 Definitions and Acronyms\nList definitions and acronyms that need to be defined in order to ensure proper interpretation of the process or procedure.\n1.5 References\n1.5.1 Standards\nList the standards (Audit, governing body etc) used to create the process/procedure.\n1.5.2 Forms\nList any logs or records associated with this procedure/document.\n    1.5.3 Equipment\n2. Procedure\n2.1\nAuthor\n[AUTHOR]\nProcedure\nApproved by:\n[APPROVER]\nReplaces: Version\nPage 1 of 3\n    1.0\n    \n    Company Logo Address\nNumber:\nSOP-ALL-\n2.2\n2.3\n3. Review\nTitle:\nAssurance GDS\nRevision Date:\nEffective:\nMM/DD/YY\n    Desc.\nStep 1 , Step 2 .\nProcedure\nDesc.\nStep 1 . Step 2 .\nProcedure\nDesc.\nStep 1 . Step 2 .\n    4. Nomenclature of Documents PROC - DEPT - #\nPROC:\nDOC- (Documents) Programs, Policies etc\nFORM- Logs, anything that needs filling in/completion SOP- Standard Operating Procedures\nWI- Work Instructions\nHACCP- HACCP Documents\n#: (Number)\nThree digit number, if documents are related use same first digit and sequential third digits.\nHACCP documents are numbered as follows and can include a letter to signify the specific flow chart.\n100 Product Description\nAuthor Approved by: Replaces: Version\n[AUTHOR] [APPROVER]\n    1.0\nPage 2 of 3\n    \nCompany Logo Address\nNumber: Title:\nSOP-ALL- Assurance GDS\n200 Hazard Analysis 300 Flow Chart\n400 HACCP Plan\nDOCUMENT NUMERATION:\n0##- Miscellaneous\n100- Quality Control\n200- Sanitation\n300- GMP/continuous improvement 400- Allergen Control\n500- Foreign Material Control 600- Recall and Traceability 700- Food Defense/ Emergency 800- Product Protection\n900- Supplier Approval\nRevision Date:\nEffective:\nMM/DD/YY\n    \n\n\n                    `;
 (async function main() {
     Promise.all([
-        // testPdfGenerator_createsFile(),
-        // testFileSystemNamingConvention(),
-        // testAddText_createsAFileWithText(),
-        // testSaveGuide_createsHeaderAndFooterOnEachPage(),
+        testSaveGuide_createsFile(),
+        testFileSystemNamingConvention(),
+        testSaveGuideWithOptions_addsText(),
+        testSaveGuide_addsHeaderAndFooter(),
         testSaveGuide_formatsRequiredFields(),
     ]).then((value) => { console.log('🏖  All tests passed. ✅'); });
 })().catch((error) => console.error(error));
 /**
- * Test pdf generator creates a file on the local filesystem. @function deleteFile contains test assertions.
+ * Create file with all options. Requires visual inspection, so the file is not deleted by default.
  */
 async function testSaveGuide_formatsRequiredFields() {
     let pdf = new PDFGenerator('testSaveGuide_formatsRequiredFields');
@@ -40,9 +40,9 @@ async function testSaveGuide_formatsRequiredFields() {
     // await deleteFile(pdf.filePath);
 }
 /**
- * Make sure there is a header and footer on every page.
+ * Test there is a header and footer on every page.
  */
-async function testSaveGuide_createsHeaderAndFooterOnEachPage() {
+async function testSaveGuide_addsHeaderAndFooter() {
     let pdf = new PDFGenerator('testSaveGuide_createsHeaderAndFooterOnEachPage');
     await pdf.saveGuide({
         introduction: MOCK_TEXT_LONG,
@@ -74,15 +74,15 @@ async function testSaveGuide_createsHeaderAndFooterOnEachPage() {
 /**
  * Test pdf generator creates a file on the local filesystem. @function deleteFile contains test assertions.
  */
-async function testPdfGenerator_createsFile() {
+async function testSaveGuide_createsFile() {
     let pdf = new PDFGenerator('testPdfGenerator_createsFile');
     await pdf.saveGuide();
     await deleteFile(pdf.filePath);
 }
 /**
- * Test that the pdf generator creates a file on the local filesystem
+ * Test that saving a guide with options creates a file with more text than a file without options.
  */
-async function testAddText_createsAFileWithText() {
+async function testSaveGuideWithOptions_addsText() {
     let pdf1 = new PDFGenerator('testPdfGenerator_createsFile1');
     await pdf1.saveGuide();
     let pdf2 = new PDFGenerator('testPdfGenerator_createsFile2');
@@ -106,8 +106,8 @@ async function testFileSystemNamingConvention() {
     await deleteFile(pdf2.filePath);
 }
 /**
- * Delete file. This inherently also checks if the file exists and other errors in the delete process.
- * @param {string} filePath Path to the file including its name and extension
+ * Delete file pn the local filesystem. This inherently also checks if the file exists. Assertions are made during the delete process.
+ * @param {string} filePath Relative path to the file including its name and extension.
  */
 async function deleteFile(filePath) {
     return new Promise((resolve, reject) => {
